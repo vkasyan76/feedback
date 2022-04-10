9165b93e8d6a6c2c2b6be6df2e482444b05b1942
vkasyan76 <vkasyan@gmx.de>
2022-04-10 21:15:20 +0200

    add FeedbackForm and FeedbackList

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -11,36 +11,36 @@ function FeedbackList() {
     return <p>No Feedback Yet</p>
   }
 
-  // return (
-  //   <div className="feedback-list">
-  //     <AnimatePresence>
-  //       {feedback.map((item) => (
-  //         <motion.div
-  //           key={item.id}
-  //           initial={{ opacity: 0 }}
-  //           animate={{ opacity: 1 }}
-  //           exit={{ opacity: 0 }}
-  //         >
-  //           <FeedbackItem key={item.id} item={item} />
-  //         </motion.div>
-  //       ))}
-  //     </AnimatePresence>
-  //   </div>
-  // )
-
-  // Return w/o Animation:
   return (
     <div className="feedback-list">
-      {feedback.map((item) => (
-        // <div>{item.rating}</div>
-        <FeedbackItem
-          key={item.id}
-          item={item}
-          // handleDelete={(id) => console.log(id)}
-        />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem key={item.id} item={item} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   )
+
+  // Return w/o Animation:
+  // return (
+  //   <div className="feedback-list">
+  //     {feedback.map((item) => (
+  //       // <div>{item.rating}</div>
+  //       <FeedbackItem
+  //         key={item.id}
+  //         item={item}
+  //         // handleDelete={(id) => console.log(id)}
+  //       />
+  //     ))}
+  //   </div>
+  // )
 }
 
 // No longer need as we use FeedbackContext.js:
